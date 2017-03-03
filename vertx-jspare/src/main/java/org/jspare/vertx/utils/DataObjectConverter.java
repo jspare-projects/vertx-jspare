29936377af68a08f5083bf9fff2e0f39ba1a80d4
Paulo Ferreira <paulo.ferreira@senior.com.br>
2017-03-02 23:38:11 -0300

    Utility class to serialize and deserialize data objects without converter.

---
+++ vertx-jspare/src/main/java/org/jspare/vertx/utils/DataObjectConverter.java
@@ -0,0 +1,103 @@
+/*
+ * Copyright 2016 JSpare.org.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package org.jspare.vertx.utils;
+
+import java.util.Arrays;
+
+import io.vertx.codegen.annotations.DataObject;
+import io.vertx.core.json.Json;
+import io.vertx.core.json.JsonObject;
+import lombok.experimental.UtilityClass;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@UtilityClass
+public class DataObjectConverter {
+
+  /**
+   * Serialize Object to JsonObject
+   * <p>
+   * Merge in another JSON object, this is the equivalent of putting all the
+   * entries of the other JSON object into this object.
+   * </p>
+   *
+   * @param obj
+   *          the obj
+   * @param json
+   *          the json
+   * @return the json object
+   */
+  public JsonObject toJson(Object obj, JsonObject json) {
+    json = json.mergeIn(toJson(obj));
+    return json;
+  }
+
+  /**
+   * Serialize Object to JsonObject
+   *
+   * @param obj
+   *          the obj
+   * @return the json object
+   */
+  public JsonObject toJson(Object obj) {
+    if (obj.getClass().getAnnotation(DataObject.class) == null) {
+
+      throw new IllegalArgumentException();
+    }
+    return new JsonObject(Json.encode(obj));
+  }
+
+  /**
+   * From json.
+   *
+   * @param <T> the generic type
+   * @param obj the obj
+   * @param json the json
+   * @return the t
+   */
+  public <T> T fromJson(JsonObject json, T obj) {
+
+    String encode = json.encode();
+    @SuppressWarnings("unchecked")
+    T t = (T) Json.decodeValue(encode, obj.getClass());
+    
+    Arrays.asList(obj.getClass().getDeclaredFields()).forEach(field -> {
+      try {
+
+        field.setAccessible(true);
+        Object value = field.get(t);
+        field.set(obj, value);
+      } catch (IllegalArgumentException | IllegalAccessException e) {
+        log.warn("Ignoring field {} because has follow error: {}", field.getName(), e.getMessage());
+        log.error(e.getMessage(), e);
+      }
+    });
+    return obj;
+  }
+  
+  /**
+   * From json.
+   *
+   * @param <T> the generic type
+   * @param json the json
+   * @param clazz the clazz
+   * @return the t
+   */
+  public <T> T fromJson(JsonObject json, Class<T> clazz){
+    String encode = json.encode();
+    return Json.decodeValue(encode, clazz);
+  }
+}
